846b15c8b96ed81cdc501b4db6fd9116723f9411
Dylan Awalt-Conley <web@dylan.ac>
2020-06-04 20:07:28 -0400

    removed sass library, just keeping as sass files

--- index.js
+++
@@ -1,8 +0,0 @@
-const sass = require('sass')
-const fs = require('fs')
-
-const result = sass.renderSync({
-    file: '_index.scss',
-    includePaths: [ 'partials' ]
-})
-fs.writeFileSync('main.css', result.css)
