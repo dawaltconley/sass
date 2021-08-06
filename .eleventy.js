7a6935b86013823da112e84836e3397c7417e43d
Dylan Awalt-Conley <web@dylan.ac>
2021-08-05 23:29:04 -0400

    added assets from previous starter site to test sass

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,20 @@
+const yaml = require('js-yaml');
+
 module.exports = eleventyConfig => {
     eleventyConfig.addWatchTarget('eleventy/css');
+    eleventyConfig.addPassthroughCopy('eleventy/assets');
+
+    // add YAML support
+    eleventyConfig.addDataExtension('yml', data => yaml.safeLoad(data));
+    eleventyConfig.addDataExtension('yaml', data => yaml.safeLoad(data));
 
     return {
         dir: {
             input: './eleventy',
             output: './eleventy/_site',
+            layouts: '_layouts',
+            data: '_data',
         },
-        htmlTemplateEngine: 'njk'
+        htmlTemplateEngine: 'liquid'
     };
 };
